cfcce1da3de638f74ffe1e3503c27114a76ad371
Singulart <serpian@gmail.com>
2020-04-17 14:35:57 +0300

    corpus maker script

--- biorxiv_dataset.py
+++ biorxiv_dataset.py
@@ -1,1 +1,146 @@
+import os
+import json
+from pathlib import Path
+from pprint import pprint
+from copy import deepcopy
 
+from tqdm.notebook import tqdm
+
+
+def format_name(author):
+    middle_name = " ".join(author['middle'])
+
+    if author['middle']:
+        return " ".join([author['first'], middle_name, author['last']])
+    else:
+        return " ".join([author['first'], author['last']])
+
+
+def format_affiliation(affiliation):
+    text = []
+    location = affiliation.get('location')
+    if location:
+        text.extend(list(affiliation['location'].values()))
+
+    institution = affiliation.get('institution')
+    if institution:
+        text = [institution] + text
+    return ", ".join(text)
+
+
+def format_authors(authors, with_affiliation=False):
+    name_ls = []
+
+    for author in authors:
+        name = format_name(author)
+        if with_affiliation:
+            affiliation = format_affiliation(author['affiliation'])
+            if affiliation:
+                name_ls.append(f"{name} ({affiliation})")
+            else:
+                name_ls.append(name)
+        else:
+            name_ls.append(name)
+
+    return ", ".join(name_ls)
+
+
+def format_body(body_text):
+    texts = [(di['section'], di['text']) for di in body_text]
+    texts_di = {di['section']: "" for di in body_text}
+
+    for section, text in texts:
+        texts_di[section] += text
+
+    body = ""
+
+    for section, text in texts_di.items():
+        body += section
+        body += "\n\n"
+        body += text
+        body += "\n\n"
+
+    return body
+
+
+def format_bib(bibs):
+    if type(bibs) == dict:
+        bibs = list(bibs.values())
+    bibs = deepcopy(bibs)
+    formatted = []
+
+    for bib in bibs:
+        bib['authors'] = format_authors(
+            bib['authors'],
+            with_affiliation=False
+        )
+        formatted_ls = [str(bib[k]) for k in ['title', 'authors', 'venue', 'year']]
+        formatted.append(", ".join(formatted_ls))
+
+    return "; ".join(formatted)
+
+
+def load_files(dirname):
+    filenames = os.listdir(dirname)
+    raw_files = []
+
+    for filename in tqdm(filenames):
+        filename = dirname + filename
+        file = json.load(open(filename, 'rb'))
+        raw_files.append(file)
+
+    return raw_files
+
+
+def generate_clean_df(all_files):
+    cleaned_files = []
+
+    for file in tqdm(all_files):
+        features = [
+            file['paper_id'],
+            file['metadata']['title'],
+            format_authors(file['metadata']['authors']),
+            format_authors(file['metadata']['authors'],
+                           with_affiliation=True),
+            format_body(file['abstract']),
+            format_body(file['body_text']),
+            format_bib(file['bib_entries']),
+            file['metadata']['authors'],
+            file['bib_entries']
+        ]
+
+        cleaned_files.append(features)
+
+    col_names = ['paper_id', 'title', 'authors',
+                 'affiliations', 'abstract', 'text',
+                 'bibliography', 'raw_authors', 'raw_bibliography']
+
+    clean_df = pd.DataFrame(cleaned_files, columns=col_names)
+    clean_df.head()
+
+    return clean_df
+
+
+biorxiv_dir = 'biorxiv_medrxiv/pdf_json/'
+filenames = os.listdir(biorxiv_dir)
+print("Number of articles retrieved from biorxiv:", len(filenames))
+
+all_files = []
+
+for filename in filenames:
+    filename = biorxiv_dir + filename
+    file = json.load(open(filename, 'rb'))
+    all_files.append(file)
+
+with open('corpus.txt', 'w', encoding='utf-8') as corpus:
+    for file in all_files:
+        print("body_text content:")
+        # pprint(file['body_text'][:2], depth=3)
+
+        texts = [(di['section'], di['text']) for di in file['body_text']]
+        texts_di = {di['section']: "" for di in file['body_text']}
+        # for section, text in texts:
+        #     texts_di[section] += text
+
+        # pprint(list(texts_di.keys()))
+        corpus.write(format_body(file['body_text']))
